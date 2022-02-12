3dfb2c623ba3654fa4c4c3cc71896ca96b66befa
Zhutian <zhutian.chen.code@outlook.com>
2022-02-12 15:25:08 -0500

    update

---
+++ Assets/Exp-SelfDriving/Script/MyCar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCar : MonoBehaviour
+{
+
+    public bool myCar = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.Find("Camera").gameObject.SetActive(myCar);
+        this.transform.Find("ARLabel").gameObject.SetActive(!myCar);
+    }
+
+}
