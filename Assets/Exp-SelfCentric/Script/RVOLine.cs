e487ef0d7133d5390608f6ca46b6965aaf98c74c
Zhutian <zhutian.chen.code@outlook.com>
2022-02-15 20:33:15 -0500

    fix: a bug + line intersection

--- Assets/Exp-SelfCentric/Script/RVOLine.cs
+++ Assets/Exp-SelfCentric/Script/RVOLine.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
@@ -22,5 +23,47 @@ public class RVOLine : MonoBehaviour
         Vector3 start = new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z);
         line.SetPosition(0, start);
         line.SetPosition(1, end.position);
+    }
+
+    Vector3[] points()
+    {
+        Vector3 start = new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z);
+        return new Vector3[] { start, end.position };
+    }
+
+    public bool isIntersected(RVOLine line, Camera cam)
+    {
+        Vector3[] self = points();
+        Vector3[] other = line.points();
+
+        Vector2[] selfInCam = self.Select(v => cam.WorldToScreenPoint(v)).Select(v => new Vector2(v.x, v.y)).ToArray();
+        Vector2[] otherInCam = other.Select(v => cam.WorldToScreenPoint(v)).Select(v => new Vector2(v.x, v.y)).ToArray();
+        Vector2 intersection = Vector2.zero;
+        return LineSegmentsIntersection(selfInCam[0], selfInCam[1], otherInCam[0], otherInCam[1], out intersection);
+    }
+
+    public static bool LineSegmentsIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, out Vector2 intersection)
+    {
+        intersection = Vector2.zero;
+
+        var d = (p2.x - p1.x) * (p4.y - p3.y) - (p2.y - p1.y) * (p4.x - p3.x);
+
+        if (d == 0.0f)
+        {
+            return false;
+        }
+
+        var u = ((p3.x - p1.x) * (p4.y - p3.y) - (p3.y - p1.y) * (p4.x - p3.x)) / d;
+        var v = ((p3.x - p1.x) * (p2.y - p1.y) - (p3.y - p1.y) * (p2.x - p1.x)) / d;
+
+        if (u < 0.0f || u > 1.0f || v < 0.0f || v > 1.0f)
+        {
+            return false;
+        }
+
+        intersection.x = p1.x + u * (p2.x - p1.x);
+        intersection.y = p1.y + u * (p2.y - p1.y);
+
+        return true;
     }
 }
