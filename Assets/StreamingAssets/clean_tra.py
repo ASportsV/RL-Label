06c63a7cdc52e87aafe7bf8178d9377b2c340541
zhutian.chen <zhutian.chen.code@outlook.com>
2022-04-24 19:38:42 -0400

    update clean

--- Assets/StreamingAssets/clean_tra.py
+++ Assets/StreamingAssets/clean_tra.py
@@ -5,11 +5,17 @@ with open(STU) as stuf:
     records = stuf.readlines()
     for record in records:
         trackIdx, currentStep, playerIdx, px, py = record.split(',')
-        if trackIdx != '5':
-            clean_records.append(record)
-        else:
+        if trackIdx == '5':
             if playerIdx not in ['123','132', '133', '138', '141', '143', '144', '145', '120', '130', '121', '124', '136', '157', '154', '160', '121', '146', '150', '149']:
                 clean_records.append(record)
+        elif trackIdx == '4':
+            if playerIdx not in ['20', '21', '22', '23', '24', '30', '29', '4', '14']:
+                clean_records.append(record)
+        elif trackIdx == '3':
+            if playerIdx not in ['7', '8']:
+                clean_records.append(record)
+        else :
+            clean_records.append(record)
 
 with open('./clean_tra.csv', 'w') as stuf:
     stuf.write(''.join(clean_records))
